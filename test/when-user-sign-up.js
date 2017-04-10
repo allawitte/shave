e7113d0982590e1dca4ab70bde7bdb41e2c318d6
Alla Klimova <alla_88@inbox.lv>
2017-04-10 13:25:34 -0300

    user auth with email and password return success - failed

--- test/when-user-sign-up.js
+++ test/when-user-sign-up.js
@@ -1,12 +1,23 @@
 'use strict';
 var assert = require('chai').assert;
 const server = require('../src/server');
-suite('When user sign up', function() {
+suite('When user sign up', function () {
     suite(' with empty input', function () {
         test('- returns error', function () {
             let userCredentials = {};
             let result = server.post('/register/', userCredentials);
             assert.equal(result, 'error');
+        });
+    });
+
+    suite(' with email and password', function () {
+        test(' - returns success', function () {
+            let userCredentials = {
+                email: 'test@test.com',
+                password: '123456'
+            };
+            let result = server.post('/register/', userCredentials);
+            assert.equal(result, 'success');
         });
     });
 });
