f3093912bc91dccf7f0545b9e850d33be260a10b
Alla Klimova <alla_88@inbox.lv>
2017-04-10 12:58:51 -0300

    user auth with empty data and error return - works

---
+++ src/server.js
@@ -0,0 +1,12 @@
+'use strict';
+var app = {};
+app.post = function(route, data){
+    if(Object.keys(data).length == 0){
+        return 'error';
+    }
+}
+module.exports = app;
+
+/**
+ * Created by HP on 4/9/2017.
+ */
