7b2b93ea87cbeeb42813a85c53a6da4ddc74aece
Alla Klimova <alla_88@inbox.lv>
2017-04-13 21:46:31 -0300

    delivery to a bunch of subscribed users - failed

--- test/when-user-subscribed.js
+++ test/when-user-subscribed.js
@@ -25,6 +25,39 @@ suite('When user is subscribed', function () {
     });
 
 });
+suite('When few user are subscribed', function () {
+    test('- delivery record is added to each of them', function () {
+        let subscriptions = [{
+            "_id": "58ee6dacee03651b3cf44b4e",
+            "userId": "58ec28357d44c73f2029676e",
+            "articl": "01",
+            "data": "2017-04-12T18:10:52.550Z",
+            "active": true,
+            "delivery": [],
+            "__v": 0
+        }
+        ,{
+                "_id": "58ee6dacee03651b3cf44b4f",
+                "userId": "58ec28357d44c73f2029676f",
+                "articl": "01",
+                "data": "2017-04-12T18:10:52.550Z",
+                "active": true,
+                "delivery": [],
+                "__v": 0
+            }
+        ,{
+                "_id": "58ee6dacee03651b3cf44b4g",
+                "userId": "58ec28357d44c73f2029676g",
+                "articl": "01",
+                "data": "2017-04-12T18:10:52.550Z",
+                "active": true,
+                "delivery": [],
+                "__v": 0
+            }];
+        let result = deliveryToUser(subscriptions);
+        assert.equal(result.report.length, subscriptions.length);
+    });
+});
 /**
  * Created by HP on 4/12/2017.
  */
